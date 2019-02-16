cbf3f53228f16ee9ba12ef57a5edc1056b909bf3
pankaj kumar chaurasia <impankaj2108@gmail.com>
2019-02-16 14:32:56 +0530

    fastest solution for this probelm

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -21,3 +21,32 @@ vector<vector<int>> threeSum(vector<int>& nums) {
         return vect;
     }
 
+
+
+
+
+*******best solution *******
+
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int> > vect;
+        sort(nums.begin(),nums.end());
+        for(int x=0;x<nums.size();x++){
+            int i=x+1,j=nums.size()-1;
+            int target=nums[x];
+            while(i<j){
+                    if(nums[i]+nums[j]+target==0)
+                    {
+                        vect.push_back({target,nums[i],nums[j]});
+                        while(i<j && nums[i]==nums[i+1])    i++;
+                        while(i<j && nums[j]==nums[j-1])    j--;
+                        i++;
+                        j--;
+                    }
+                    else if(nums[i]+nums[j]+target>0)  j--;
+                    else    i++;
+                }
+            while(x+1<nums.size() && nums[x]==nums[x+1])    x++;
+            }
+        return vect;
+    }
