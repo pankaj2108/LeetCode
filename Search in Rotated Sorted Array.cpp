bbbfc69f6931f3b90a493f2e82f57d6d588a2772
pankaj kumar chaurasia <impankaj2108@gmail.com>
2019-02-20 03:00:00 +0530

    documentation

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -3,13 +3,13 @@ int search(vector<int>& nums, int target) {
         while(l<=r){
             int mid=l+(r-l)/2;
             if(nums[mid]==target)   return mid;
-            if(nums[l]<=nums[mid]){
+            if(nums[l]<=nums[mid]){     //if array nums[l..mid] is sorted
                 if(target>=nums[l] && target<=nums[mid])
                     r=mid-1;
                 else
                     l=mid+1;
             }
-            else{
+            else{       // if array nums[mid..r] is not sorted
                 if(target>=nums[mid] && target<=nums[r])
                     l=mid+1;
                 else
